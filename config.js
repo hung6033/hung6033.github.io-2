f1aaf85426c14f2f2897694c93fb3d6c497344c0
hung6033 <90978454+hung6033@users.noreply.github.com>
2021-09-19 00:24:30 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    introTitle: 'Em làm bạn chị được không?',
+    introDesc: `Dẫu biết rằng chị ghét em đến nhường nào nhưng chị có thể quay lại làm bạn em không..`,
+    btnIntro: 'Nha',
+    title: 'Em nói em sẽ quên chị nhưng nó thật sự rất khó',
+    desc: 'Nên chị làm bạn với em đuọc không ',
+    btnYes: 'Cũng được :>',
+    btnNo: 'Không :< ',
+    question:'Chị sẽ tha thứ cho em chứ?',
+    btnReply: 'Gửi',
+    reply: 'Có chứ :>',
+    mess: 'Em cảm ơn nhiềuuu :(((',
+    messDesc: 'Chị sẽ nhắn tin với em chứ :(((',
+    btnAccept: 'Okayy',
+    messLink: 'http://fb.com/messages/t/100051992931764' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
